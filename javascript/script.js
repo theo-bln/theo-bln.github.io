c27740c4502952a4b50f457d5075bdf8582d687a
Theostreiber <theo_streiber@icloud.com>
2023-07-03 14:02:57 +0200

    fix java script

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,14 @@
 /* Here we will write our Javascript code */
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    console.log(top);
+    if(top >=100) {
+        header.classList.add('bg-dark');
+    }
+    else {
+        header.classList.remove('bg-dark');
+    }
+}
