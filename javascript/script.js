f593d7b0d54fdb553a693e63a9e702846a93f8ff
Theostreiber <theo_streiber@icloud.com>
2023-07-06 12:27:39 +0200

    color change

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,6 @@ const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
     var top = window.scrollY;
-    console.log(top);
     if(top >=800) {
         header.classList.add('bg-dark');
     }
