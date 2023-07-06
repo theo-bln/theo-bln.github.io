f2cc7bf46aa04e1ef76ad06d1794409e050e7a97
Theostreiber <theo_streiber@icloud.com>
2023-07-06 09:28:33 +0200

    scroll spy

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,7 @@ const header = document.querySelector('.navbar');
 window.onscroll = function() {
     var top = window.scrollY;
     console.log(top);
-    if(top >=900) {
+    if(top >=800) {
         header.classList.add('bg-dark');
     }
     else {
